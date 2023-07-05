260556e601425c4d0b0e8619af79806aa0744ea7
pra373 <69187354+pra373@users.noreply.github.com>
2023-07-05 18:06:48 +0530

    Add files via upload

---
+++ Introduction_Demo/Letter_P.cpp
@@ -0,0 +1,301 @@
+// Header files 
+#include <GL/freeglut.h>
+
+// Global variable declarations
+bool bIsFullScreen = false;
+
+// Entry-point function
+int main(int argc, char *argv[])
+{
+	// Function declarations
+	void initialize(void);
+	void resize(int, int);
+	void display(void);
+	void keyboard(unsigned char, int, int);
+	void mouse(int, int, int, int);
+	void uninitialized(void);
+
+	// Code
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("My First RTR5 Programe : Prathamesh Laxmikant Paropkari");
+
+	initialize();
+
+	glutReshapeFunc(resize);
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
+	glutCloseFunc(uninitialized);
+
+	glutMainLoop();   // heart of application
+
+	return 0;
+
+}
+
+void initialize(void)
+{
+	// code
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void resize(int width, int height)
+{
+	// code
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+}
+
+void display(void)
+{
+	// code
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glBegin(GL_LINES);
+
+	/*glColor3f(1.0f, 0.0f, 0.0f);   
+	glVertex3f(0.59f, 0.54f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.345f, 0.8f, 0.0f);
+										   
+	glColor3f(0.0f, 1.0f, 0.0f);       
+	glVertex3f(-0.6f, 0.8f, 0.0f);
+
+
+
+	glColor3f(0.0f, 0.0f, 1.0f);          // commented code is for GL_POLYGON, please ignore it..
+	glVertex3f(-0.6f, 0.44f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, 0.44f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, -0.8f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.8f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.059f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.059f, -0.195f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.35f, -0.195f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.59f, 0.055f, 0.0f);  */
+
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.59f, 0.54f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.345f, 0.8f, 0.0f);  //line 1 end
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(0.345f, 0.8f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.6f, 0.8f, 0.0f);// line 2 end
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.6f, 0.8f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, 0.44f, 0.0f); //line 3 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, 0.44f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, 0.44f, 0.0f); //line 4 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, 0.44f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.445f, 0.0f);// line 5 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.5f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, -0.445f, 0.0f); //line 6 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, -0.8f, 0.0f);//line 7 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.6f, -0.8f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.8f, 0.0f); //line 8 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.8f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.445f, 0.0f);//line 9 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.059f, -0.445f, 0.0f); // line 10 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.059f, -0.445f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.059f, -0.195f, 0.0f); //line 11 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.059f, -0.195f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.35f, -0.195f, 0.0f);//line 12 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.35f, -0.195f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.59f, 0.055f, 0.0f); // line 13 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.59f, 0.055f, 0.0f);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.59f, 0.54f, 0.0f); //line 14 end
+
+
+	glEnd();
+
+	glBegin(GL_LINES);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.26f, 0.43f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.16f, 0.525f, 0.0f); //line 1 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.16f, 0.525f, 0.0f);
+	
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.158f, 0.525f, 0.0f);//line 2 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.158f, 0.525f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.158f, 0.07f, 0.0f); // line 3 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.158f, 0.07f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.16f, 0.07f, 0.0f); //line 4 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.16f, 0.07f, 0.0f); 
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.26f, 0.17f, 0.0f); // line 5 end
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.26f, 0.17f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(0.26f, 0.43f, 0.0f); //line 6 end
+
+
+
+
+
+
+
+
+
+	glEnd();
+	glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	// code
+	switch (key)
+	{
+	case 27:
+		glutLeaveMainLoop();
+		break;
+	case 'F':
+	case 'f':
+		if (bIsFullScreen == false)
+		{
+			glutFullScreen();
+			bIsFullScreen = true;
+		}
+		else
+		{
+			glutLeaveFullScreen();
+			bIsFullScreen = false;
+		}
+		break;
+
+	default:
+		break;
+
+	}
+
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	// code
+	switch (button)
+	{
+	case GLUT_RIGHT_BUTTON:
+		glutLeaveMainLoop();
+		break;
+	default:
+		break;
+	}
+
+}
+
+void uninitialized(void)
+{
+	// code
+
+}
+
+
+
+
+
