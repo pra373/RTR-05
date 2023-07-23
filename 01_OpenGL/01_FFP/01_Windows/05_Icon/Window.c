36515e64d215ef02f910e9aae78d425556901ef9
pra373 <prathameshparopkari321@gmail.com>
2023-07-24 01:44:41 +0530

    Add Icon To Window

---
+++ 01_OpenGL/01_FFP/01_Windows/05_Icon/Window.c
@@ -0,0 +1,102 @@
+//Windows header files
+
+#include<windows.h>
+#include "window.h"
+
+// Global Function declarations
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Entry point function
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// Local variable declaration
+
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("PLP Window");
+
+	//code
+	
+	// WNDCLASSEX Initialization
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbWndExtra = 0;
+	wndclass.cbClsExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	RegisterClassEx(&wndclass);
+
+	//create window
+
+	hwnd = CreateWindow(szAppName,
+		TEXT("Prathamesh Laxmikant Paropkari"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	// Show the window
+
+	ShowWindow(hwnd, iCmdShow);
+
+	// Paint or Redraw the window
+
+	UpdateWindow(hwnd);
+
+	// message loop
+
+	while (GetMessage(&msg, NULL, 0, 0))   // heart of application
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return((int)msg.wParam);
+
+}
+
+// call back function
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	// code
+
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+
+		
+		
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam,lParam));
+	
+
+	
+}
+
+
+
+
