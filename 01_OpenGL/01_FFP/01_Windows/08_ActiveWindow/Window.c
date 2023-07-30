8c067d438c2cae3baf8fd2b725b9e17a35c7c62c
pra373 <prathameshparopkari321@gmail.com>
2023-07-30 16:08:11 +0530

    Active - Inactive Window Program

---
+++ 01_OpenGL/01_FFP/01_Windows/08_ActiveWindow/Window.c
@@ -0,0 +1,239 @@
+//Windows header files
+
+#include<windows.h>
+#include<stdio.h>              // for file I/O
+#include<stdlib.h>             // for exit()
+#include "window.h"
+
+// Global Function declarations
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//global variable declarations
+
+FILE *gpFile = NULL;           // (gpFile) global pointer to a file
+
+HWND ghwnd = NULL;             // handle of window
+BOOL gbActive = FALSE;
+DWORD dwStyle = 0;
+WINDOWPLACEMENT wpPrev = { sizeof(WINDOWPLACEMENT) };
+BOOL gbFullScreen = FALSE;
+
+//Entry point function
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	// Local variable declaration
+
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("PLP Window");
+
+	int ScreenWidth;
+	int ScreenHeight;
+	int x_screen_center;
+	int y_screen_center;
+
+	//code
+
+	ScreenWidth = GetSystemMetrics(SM_CXSCREEN);
+	ScreenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	x_screen_center = ScreenWidth / 2;
+	y_screen_center = ScreenHeight / 2;
+
+	gpFile = fopen("Log.txt", "w");
+	if (gpFile == NULL)
+	{
+		MessageBox(NULL, TEXT("Log file cannot be opened"), TEXT("ERROR"), MB_OK);
+		exit(0);
+	}
+
+	fprintf(gpFile, "Program startrd successfully\n");
+	
+	// WNDCLASSEX Initialization
+
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbWndExtra = 0;
+	wndclass.cbClsExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(MYICON));
+
+	RegisterClassEx(&wndclass);
+
+	//create window
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		szAppName,
+		TEXT("Prathamesh Laxmikant Paropkari"),
+		WS_OVERLAPPEDWINDOW,
+		(x_screen_center - 400),
+		(y_screen_center - 300),
+		800,
+		600,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ghwnd = hwnd;
+
+	// Show the window
+
+	ShowWindow(hwnd, iCmdShow);
+
+	// Paint or Redraw the window
+
+	UpdateWindow(hwnd);
+
+	// message loop
+
+	while (GetMessage(&msg, NULL, 0, 0))   // heart of application
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return((int)msg.wParam);
+
+}
+
+// call back function
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//Function declaration
+
+	void ToggleFullScreen(void);
+
+	// code
+
+	switch (iMsg)
+	{
+	case WM_DESTROY:
+
+		if (gpFile)
+		{
+			fprintf(gpFile, "Program Ended successfully\n");
+			fclose(gpFile);
+			gpFile = NULL;
+
+		}
+		PostQuitMessage(0);
+		break;
+
+	case WM_SETFOCUS:
+		gbActive = TRUE;
+		break;
+
+	case WM_KILLFOCUS:
+		gbActive = FALSE;
+		break;
+
+	case WM_KEYDOWN:                 // use for non character keys
+		switch (LOWORD(wParam))
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+
+		}
+		break;
+
+	case WM_CHAR:                    // use for character keys
+		switch (LOWORD(wParam))
+		{
+		case 'F':
+		case 'f':
+			if (gbFullScreen == FALSE)
+			{
+				ToggleFullScreen();
+				gbFullScreen = TRUE;
+			}
+			else
+			{
+				ToggleFullScreen();
+				gbFullScreen = FALSE;
+
+			}
+
+			break;
+
+
+
+
+		}
+
+		break;
+
+
+	default:
+		break;
+
+		
+		
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam,lParam));
+	
+
+	
+}
+
+void ToggleFullScreen(void)
+{
+	// Local variable declarations 
+
+	MONITORINFO mi = { sizeof(MONITORINFO) };
+
+
+	//code
+
+	if (gbFullScreen == FALSE)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+
+		if (dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ghwnd, &wpPrev) && GetMonitorInfo(MonitorFromWindow(ghwnd, MONITORINFOF_PRIMARY), &mi))
+			{
+				SetWindowLong(ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW);
+				SetWindowPos(ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED);
+			}
+		}
+
+		ShowCursor(FALSE);
+	}
+
+	else
+	{
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+		ShowCursor(TRUE);
+
+	}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
