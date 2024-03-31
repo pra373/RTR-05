16f926473c528cffc22b375b5c892a6201053d64
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-04-01 02:33:10 +0530

    Diffuse Light with Sphere

---
+++ 01_OpenGL/02-PP/12 - Light/01- Diffuse Light/03 - Sphere/Build.bat
@@ -0,0 +1,10 @@
+cls
+
+cl.exe /c /EHsc /I "C:\glew-2.1.0-win32\glew-2.1.0\include" OGL.cpp  
+
+rc.exe OGL.rc 			   
+
+link.exe OGL.obj OGL.res user32.lib gdi32.lib /LIBPATH:"C:\glew-2.1.0-win32\glew-2.1.0\lib\Release\x64" /SUBSYSTEM:WINDOWS 
+
+OGL.exe
+
