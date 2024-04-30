f2aeabbbda1591a647a1a555be46cf9f0638f136
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-01 01:17:03 +0530

    3D Texture Programs

---
+++ 01_OpenGL/02-PP/01-Windows/11 - 3D Texture/01 - 2 Shapes/OGL.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define MYICON 100
+#define MY_STONE_BITMAP 200
+#define MY_KUNDALI_BITMAP 300
+
+
---
+++ 01_OpenGL/02-PP/01-Windows/11 - 3D Texture/03 - Single Cube/OGL.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define MYICON 100
+
+#define MY_KUNDALI_BITMAP 300
+
+
