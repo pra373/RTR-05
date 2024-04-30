f75b5a43a65caed2f6567a2923e9fc6ef37e090b
Prathamesh  Paropkari <prathameshparopkari321@gmail.com>
2024-05-01 01:18:39 +0530

    Delete 01_OpenGL/02-PP/11 - 3D Texture directory

--- 01_OpenGL/02-PP/11 - 3D Texture/01 - 2 Shapes/OGL.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MYICON 100
-#define MY_STONE_BITMAP 200
-#define MY_KUNDALI_BITMAP 300
-
-
--- 01_OpenGL/02-PP/11 - 3D Texture/02 -  Single Pyramid/OGL.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MYICON 100
-#define MY_STONE_BITMAP 200
-
-
-
