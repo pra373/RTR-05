4957b1e0452c5950dafb9c0f5cc2723c3347a7ad
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-28 00:34:09 +0530

    Textured Pyramid

---
+++ 01_OpenGL/02-PP/03-Android/OpenGL_ES/07-Texture/02-3D Texture/01-SingleShape/01-Pyramid/app/src/main/java/com/PLP/textured_pyramid/VertexAttributesEnum.java
@@ -0,0 +1,7 @@
+package com.PLP.textured_pyramid;
+
+public class VertexAttributesEnum
+{
+	public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_TEXTURE = 1;
+}
