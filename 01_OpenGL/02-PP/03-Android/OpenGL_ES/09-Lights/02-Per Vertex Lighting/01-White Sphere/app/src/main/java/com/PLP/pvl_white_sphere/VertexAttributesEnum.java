1a8aa3dfd2287b5d63ce7f25fa61736a2724833e
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-28 00:53:09 +0530

    Per Vertex Lighting White Sphere

---
+++ 01_OpenGL/02-PP/03-Android/OpenGL_ES/09-Lights/02-Per Vertex Lighting/01-White Sphere/app/src/main/java/com/PLP/pvl_white_sphere/VertexAttributesEnum.java
@@ -0,0 +1,7 @@
+package com.PLP.pvl_white_sphere;
+
+public class VertexAttributesEnum
+{
+	public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_NORMALS = 1;
+}
