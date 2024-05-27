cb2aadb2b8c7b8155b31ce337e320e5b09d0da55
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-28 01:07:10 +0530

    Two Static Lights Per Fragment

---
+++ 01_OpenGL/02-PP/03-Android/OpenGL_ES/09-Lights/04-Two Lights On Moving Pyramid/02-Per Fragment/app/src/main/java/com/PLP/pfl_two_static_lights/MainActivity.java
@@ -0,0 +1,70 @@
+package com.PLP.pfl_two_static_lights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.graphics.Color;
+
+import android.content.pm.ActivityInfo;
+
+//  Packages for Fullscreen
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+
+public class MainActivity extends AppCompatActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		
+		
+		super.onCreate(savedInstanceState);
+
+		
+
+		// Fullscreen
+
+		// Get and Hide action bar
+		getSupportActionBar().hide();
+
+		// Do Fullscreen
+		WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+		// get window insets controller object.
+		WindowInsetsControllerCompat windowInsetsControllerCompat = WindowCompat.getInsetsController(getWindow(),getWindow().getDecorView());
+
+		// tell above object to hide system bars means status bar, naviation bar and caption bar and IME(input method editor).
+		windowInsetsControllerCompat.hide(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.ime());
+
+		// Do Landscape
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+		// Setting background color to black..
+		getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+
+		GLESView glesView = new GLESView(this);
+		
+		
+		setContentView(glesView);
+	}
+
+	@Override
+
+	protected void onPause()
+	{
+		super.onPause();
+	}
+
+	@Override
+
+	protected void onResume()
+	{
+		super.onResume();
+	}
+}
+
---
+++ 01_OpenGL/02-PP/03-Android/OpenGL_ES/09-Lights/04-Two Lights On Moving Pyramid/02-Per Fragment/app/src/main/java/com/PLP/pfl_two_static_lights/VertexAttributesEnum.java
@@ -0,0 +1,7 @@
+package com.PLP.pfl_two_static_lights;
+
+public class VertexAttributesEnum
+{
+	public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_NORMALS = 1;
+}
