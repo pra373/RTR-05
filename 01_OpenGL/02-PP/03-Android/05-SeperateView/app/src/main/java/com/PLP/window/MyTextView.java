dfcd34c934b2d9f89606a5b47a1ca76b9cf7d7aa
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-27 23:11:23 +0530

    Seperate View Live Code

---
+++ 01_OpenGL/02-PP/03-Android/05-SeperateView/app/src/main/java/com/PLP/window/MyTextView.java
@@ -0,0 +1,23 @@
+package com.PLP.window;
+
+import android.content.Context;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import android.graphics.Color;
+
+import android.view.Gravity;
+
+public class MyTextView extends AppCompatTextView
+{
+	public MyTextView(Context context)
+	{
+		super(context);
+
+		setTextColor(Color.rgb(0, 255, 0));
+		setTextSize(50);
+		setGravity(Gravity.CENTER);
+		setText("Hello World !!!");
+	}
+};
+
