0da1ecd8fba571450b9f008ccf78195f4f90ebc5
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-28 00:47:58 +0530

    Diffuse Light Cube

---
+++ 01_OpenGL/02-PP/03-Android/OpenGL_ES/09-Lights/01-Diffuse Light/02-cube/app/src/main/java/com/PLP/diffuselight_cube/VertexAttributesEnum.java
@@ -0,0 +1,7 @@
+package com.PLP.diffuselight_cube;
+
+public class VertexAttributesEnum
+{
+	public static final int AMC_ATTRIBUTE_POSITION = 0;
+	public static final int AMC_ATTRIBUTE_NORMAL = 1;
+}
