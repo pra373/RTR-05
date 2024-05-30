bcebe73ea9fb37784048f59adf340b1401e06044
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-05-30 20:55:14 +0530

    Hello World Live Code

---
+++ 01_OpenGL/02-PP/04-WebGL/01 - Windowing/01-Canvas/Canvas.js
@@ -0,0 +1,68 @@
+// JavaScript source code
+
+//main function
+
+function main()
+{
+    //get canvas
+    var canvas = document.getElementById("PLP");
+
+    if (!canvas)
+    {
+        console.log("Did not get canvas \n");
+
+    }
+    else
+    {
+        console.log("Got canvas successfully \n");
+    }
+
+    //get context from above canvas
+
+    var context = canvas.getContext("2d");
+
+    if (!context)
+    {
+        console.log("Did not get context Successfully \n");
+
+    }
+
+    else
+    {
+        console.log("Got context successfully \n");
+    }
+
+    //set canvas background color to black
+
+    context.fillStyle = "black";
+
+    //paint the canvas rectangle background with above color
+
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //declare the text
+    var text = "Hello World !!!";
+
+    
+
+    
+    //set the text in middle of the canvas
+
+    context.textAlign = "center";  //horizontal centre
+
+    context.textBaseLine = "middle"; //vertical centre
+
+    //set Font
+
+    context.font = "48px sans-serif";
+
+    //set text color to white
+
+    context.fillStyle = "white";
+
+    //paint the text with above color
+
+    context.fillText(text, (canvas.width / 2), (canvas.height / 2));
+
+
+}
