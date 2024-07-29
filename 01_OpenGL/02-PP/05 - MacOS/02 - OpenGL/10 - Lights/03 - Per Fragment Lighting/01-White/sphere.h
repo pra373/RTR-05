37cc06e4eaa2f5bd775f47354c0636a1508b7dd1
Prathamesh Paropkari <prathameshparopkari321@gmail.com>
2024-07-29 23:56:30 +0530

    Per Fragment Lighting

---
+++ 01_OpenGL/02-PP/05 - MacOS/02 - OpenGL/10 - Lights/03 - Per Fragment Lighting/01-White/sphere.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cmath>
+#include <vector>
+using namespace std;
+
+enum { AMC_ATTRIBUTE_POSITION, AMC_ATTRIBUTE_NORMAL, AMC_ATTRIBUTE_TEXTURE };
+
+class mesh {
+private:
+  vector<unsigned short> elements; // vector for elements
+
+  // vectors for basic data
+
+  vector<float> vertices;
+  vector<float> normals;
+  vector<float> texcoords;
+
+  int numElements = 0;
+  int maxElements = 0;
+  int numVertices = 0;
+
+  GLuint vboPosition = 0;
+  GLuint vboNormal = 0;
+  GLuint vboTexture = 0;
+
+  GLuint vboIndex = 0;
+  GLuint vao = 0;
+
+public:
+  // member functions
+
+  mesh();
+
+  void allocate(int numIndices);
+
+  // Add 3 vertices, 3 normal and 2 texcoords i.e. one triangle to the geometry.
+  // This searches the current list for identical vertices (exactly or nearly)
+  // and if one is found, it is added to the index array. if not, it is added to
+  // both the index array and the vertex array.
+
+  void addTriangle(float **, float **, float **);
+  void prepareToDraw(void);
+  void draw(void);
+  int getIndexCount(void);
+  int getVertexCount(void);
+  void normalizeVector(float[]);
+  bool isFoundIdentical(float, float, float);
+  void cleanupMeshData(void);
+
+  void deallocate(void);
+};
+
+void makeSphere(mesh &sphereMesh, float fRadius, int iSlices, int iStacks);
